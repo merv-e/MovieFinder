35e31409a85a53b39402df8ad471584fdfa443ca
merv-e <34655454+merv-e@users.noreply.github.com>
2023-07-15 22:57:00 +0200

    css, and html files are created. so is js but Im still working on it.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const API_URL = "";
+("https://api.themoviedb.org/3/discover/movie?api_key=06037b2c68e092d4ab77bebfba7db548");
+  
+  const fetchMovies = fetch(API_URL)
+  .then(data => {
+      console.log(data);
+  })
+
+ const search = document.getElementById('search');
+
+ search.addEventListener('', function (){
+
+ })
+
+ dk 03:48
