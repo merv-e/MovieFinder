7e221aa0155f919ce2bb60112d782e14ce695767
merv-e <34655454+merv-e@users.noreply.github.com>
2023-07-17 12:20:30 +0200

    daha detayli devam edilecke.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-const API_URL = "";
-("https://api.themoviedb.org/3/discover/movie?api_key=06037b2c68e092d4ab77bebfba7db548");
+const API_URL = ("https://api.themoviedb.org/3/discover/movie?api_key=06037b2c68e092d4ab77bebfba7db548");
   
+// const IMG_PATH = '';
+
+const IMG_PATH = fetch(
+  "https://api.themoviedb.org/3/configuration"
+);
+
   const fetchMovies = fetch(API_URL)
   .then(data => {
       console.log(data);
+  })
+
+  const fetchImages = IMG_PATH
+  .then(img => {
+      console.log(img);
   })
 
  const search = document.getElementById('search');
@@ -12,4 +22,8 @@ const API_URL = "";
 
  })
 
- dk 03:48
+async function getMovies(url) {
+  
+}
+
+ //dk 4. => fetching images
